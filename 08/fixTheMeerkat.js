e44baae498baadd2234e1fbeb1cc92bb621862c8
erichodges <14981329+erichodges@users.noreply.github.com>
2019-10-24 13:42:27 -0700

    Work: Added 2nd solution

--- 08/fixTheMeerkat.js
+++ 08/fixTheMeerkat.js
@@ -1,10 +1,20 @@
 
+// function fixTheMeerkat(arr) {
+//   const tail = arr[0];
+//   const body = arr[1];
+//   const head = arr[2];
+
+//   return [head, body, tail];
+// }
+
 function fixTheMeerkat(arr) {
-  const tail = arr[0];
-  const body = arr[1];
-  const head = arr[2];
+  const [tail, body, head] = arr;
 
   return [head, body, tail];
 }
+
+
+
+
 
 console.log(fixTheMeerkat(['tail', 'body', 'head']), ['head', 'body', 'tail']);
