cf886b40b5912887b0cd94d7131ad4719957d03a
erichodges <14981329+erichodges@users.noreply.github.com>
2019-10-26 13:29:49 -0700

    Study: Cleaned up sortByLength

--- 07/sortByLength.js
+++ 07/sortByLength.js
@@ -1,19 +1,19 @@
-// function sortByLength(array) {
-//   array.sort((a, b) => {
-//     if (a.length > b.length) {
-//       return 1;
-//     }
-//     if (a.length < b.length) {
-//       return -1;
-//     }
-//     return 0
-//   });
-//   return array;
-// }
-
 function sortByLength(array) {
-  return array.sort((a, b) => a.length - b.length);
-  
+  array.sort((a, b) => {
+    if (a.length > b.length) {
+      return 1;
+    }
+    if (a.length < b.length) {
+      return -1;
+    }
+    return 0
+  });
+  return array;
 }
+
+// function sortByLength(array) {
+//   return array.sort((a, b) => a.length - b.length);
+  
+// }
 
 console.log(sortByLength(["beg", "life", "i", "to"]), ["i", "to", "beg", "life"]);
