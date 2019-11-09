29fbf1f903b09a570518434f54f6b2ec644aaaf3
Eric Hodges <ehodges2004@gmail.com>
2019-11-09 15:23:16 -0800

    Added solution to find the longest gap

--- 07/find-the-longest-gap.js
+++ 07/find-the-longest-gap.js
@@ -1,33 +1,58 @@
-// this one is not working yet!!
+// // this one is not working yet!!
+// function gap(num) {
+//   // convert the number to a binary string
+//   const binaryString = num.toString(2);
+
+//   // a place to store the length of the longest segment
+//   // initialized to zero
+//   let longestSegment = 0;
+
+//   // a place to store the length of the current longest segment
+//   let currentLongestSegment = 0;
+
+//   // iterate over the binary string
+//   for (let i = 0; i < binaryString.length; i++) {
+//     const bit = binaryString[i];
+//     // if the current value is 1
+//     if (bit == 1) {
+//       //check if current longest segment is greater than overall longest segment
+//       if (currentLongestSegment > longestSegment) {
+//         // if so, update overall longest segment
+//         longestSegment = currentLongestSegment;
+//       }
+//       // reset current logest segment to 0
+//       currentLongestSegment = 0;
+//     } else {
+//       // if the current value is a 0
+//       // increment the current longest segment
+//       currentLongestSegment += 1;
+//     }
+//   }
+//   return longestSegment;
+// }
+
+// Math.max(..."100010".toString(2).match(/([0]+)/g).map(n=>n.length))
+
+// (s) => Math.max (...(s.replace(/1/g, ' ').split(' ').map (s => s.length)))
+
 function gap(num) {
-  // convert the number to a binary string
   const binaryString = num.toString(2);
 
-  // a place to store the length of the longest segment
-  // initialized to zero
   let longestSegment = 0;
-
-  // a place to store the length of the current longest segment
   let currentLongestSegment = 0;
 
-  // iterate over the binary string
   for (let i = 0; i < binaryString.length; i++) {
     const bit = binaryString[i];
-    // if the current value is 1
     if (bit == 1) {
-      //check if current longest segment is greater than overall longest segment
       if (currentLongestSegment > longestSegment) {
-        // if so, update overall longest segment
         longestSegment = currentLongestSegment;
       }
-      // reset current logest segment to 0
       currentLongestSegment = 0;
     } else {
-      // if the current value is a 0
-      // increment the current longest segment
       currentLongestSegment += 1;
     }
   }
+
   return longestSegment;
 }
 
