a1a42b41a644a27620f73bd8c303aca3616c8cda
Eric Hodges <ehodges2004@gmail.com>
2019-12-05 14:01:05 -0800

    Chore: Added solutions for flatten

--- 07/flattenArray.js
+++ 07/flattenArray.js
@@ -1,21 +1,69 @@
-function flatten(array) {
-  let flattened = [];
+// function flatten(array) {
+//   let flattened = [];
 
-  for (let i = 0; i < array.length; i++) {
-    const currentValue = array[i];
+//   for (let i = 0; i < array.length; i++) {
+//     const currentValue = array[i];
 
-    if (Array.isArray(currentValue)) {
-      for (let j = 0; j < currentValue.length; j++) {
-        const innerValue = currenValue[j];
+//     if (Array.isArray(currentValue)) {
+//       for (let j = 0; j < currentValue.length; j++) {
+//         const innerValue = currenValue[j];
 
-        flattened.push(innerValue);
-      }
-    } else {
-      flattened.push(currentValue);
-    }
-  }
-  return flattened;
-}
+//         flattened.push(innerValue);
+//       }
+//     } else {
+//       flattened.push(currentValue);
+//     }
+//   }
+//   return flattened;
+// }
+
+// function flatten(array) {
+//   let flattened = [];
+//   array.forEach(currentValue => {
+//     if (Array.isArray(currentValue)) {
+//       currentValue.forEach(innerValue => {
+//         flattened.push(innerValue);
+//       });
+//     } else {
+//       flattened.push(currentValue);
+//     }
+//   });
+//   return flattened;
+// }
+
+// function flatten(array) {
+//   let flattened = [];
+//   array.forEach(currentValue => {
+//     if (Array.isArray(currentValue)) {
+//       flattened = [...flattened, ...currentValue];
+//     } else {
+//       flattened.push(currentValue);
+//     }
+//   });
+//   return flattened;
+// }
+
+// function flatten(array) {
+//   let flattened = [];
+//   array.forEach(currentValue => {
+//     if (Array.isArray(currentValue)) {
+//       flattened = [...flattened, ...currentValue];
+//     } else {
+//       flattened = [...flattened, currentValue];
+//     }
+//   });
+//   return flattened;
+// }
+
+// function flatten(array) {
+//   return array.reduce((flattened, currentValue) => {
+//     if (Array.isArray(currentValue)) {
+//       return [...flattened, ...currentValue];
+//     }
+//     flattened.push(currentValue);
+//     return flattened;
+//   }, []);
+// }
 
 console.log(flatten([]), []);
 console.log(flatten([1, 2, 3]), [1, 2, 3]);
