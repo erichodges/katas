2beed22bf35974002a6c38954d1704e718ba9d75
Eric Hodges <ehodges2004@gmail.com>
2020-01-10 13:46:27 -0800

    Added memoized version

--- 06/Numerical-Palindrome.js
+++ 06/Numerical-Palindrome.js
@@ -27,6 +27,61 @@ function palindrome(num, palindromes = []) {
   return [...new Set(palindromes.sort((a, b) => a - b))];
 }
 
+// memoized version
+
+// const errorMessages = {
+//   notValid: 'Not valid',
+//   notFound:'No palindromes found',
+// };
+
+// function createMemoizedIsSubPalindrome() {
+//   const cache = {};
+//   return (number) => {
+//     if (number in cache) return cache[number];
+//     const strNum = number.toString();
+//     const result = strNum.split('').reverse().join('') == strNum;
+//     cache[number] = result;
+//     return result;
+//   };
+// }
+
+// const isSubPalindrome = createMemoizedIsSubPalindrome();
+
+// function createMemoizedPalindrome() {
+//   const cache = {};
+//   return (num, palindromes = []) => {
+//     let result;
+//     if (num in cache) return cache[num];
+//     if (typeof num !== 'number') {
+//       result = errorMessages.notValid;
+//     } else if (num < 0) {
+//       result = errorMessages.notValid;
+//     } else if (num < 10) {
+//       result = errorMessages.notFound;
+//     }
+
+//     if (result) {
+//       cache[num] = result;
+//       return result;
+//     }
+
+//     if (isSubPalindrome(num)) {
+//       palindromes.push(num);
+//     }
+
+//     palindrome(+num.toString().slice(1), palindromes);
+//     palindrome(+num.toString().slice(0, -1), palindromes);
+
+//     if (!palindromes.length) {
+//       cache[num] = errorMessages.notFound;
+//       return errorMessages.notFound;
+//     }
+//     return [...new Set(palindromes.sort((a, b) => a - b))];
+//   };
+// }
+
+// const palindrome = createMemoizedPalindrome();
+
 console.log(palindrome("ACCDDCCA"), "Not valid");
 console.log(palindrome("1551"), "Not valid");
 console.log(palindrome(-4505), "Not valid");
